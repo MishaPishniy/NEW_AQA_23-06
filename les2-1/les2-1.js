b666412879123135ddb3bdcb0747ee6c795f1f0c
MIsha Pyshnhyi <mishap635@gmail.com>
2026-07-03 16:37:54 +0300

    new

--- les2-1/les2-1.js
+++ les2-1/les2-1.js
@@ -1,3 +1,26 @@
-import chalk from 'chalk';
+import chalk from "chalk";
 
-console.log(chalk.blue('Hello world!'));
+let name = "Jhone Doe";
+
+console.log(chalk.blue("Hello world!"));
+console.log(chalk.blue("Hello world! "));
+console.log(chalk.blue(`Hello world! ${name}`));
+
+const val = 42;
+const pi = 3.14159;
+
+console.log(1 / 0);
+console.log(Infinity);
+console.log("number" / 2);
+console.log("number" + 2);
+const isUser = true;
+const isUser = false;
+
+let b;
+console.log(b);
+
+const data = null;
+
+const mySymbol = Symbol();
+const id1 = Symbol("id");
+
