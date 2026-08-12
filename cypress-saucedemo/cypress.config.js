fff78278e6d3fb7bd0bb343d0018805edb378805
MIsha Pyshnyi <mishap635@gmail.com>
2026-08-12 10:44:26 +0300

    cypress

---
+++ cypress-saucedemo/cypress.config.js
@@ -0,0 +1,21 @@
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+  allowCypressEnv: false,
+  viewportWidth: 1440,
+  viewportHeight: 900,
+  defaultCommandTimeout: 10000,
+  retries: {
+    runMode: 2,
+    openMode: 0,
+  },
+
+  e2e: {
+    baseUrl: 'https://www.saucedemo.com',
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+
+    
+    },
+  },
+});
